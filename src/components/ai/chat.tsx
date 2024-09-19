a3a1af647fc1a263a57cdee3e06c259968df9bc5
joelriveradev <joelriveradev@gmail.com>
2024-09-19 11:44:24 -0500

    chore: several updates to <Chat /> 1. remove faqs 2. style updates 3. add id and className props 4. integrate new <ChatInput /> 5. add syncBeforeNavigateAsync function

--- src/components/ai/chat.tsx
+++ src/components/ai/chat.tsx
@@ -1,16 +1,20 @@
 'use client'
 
-import { useEffect } from 'react'
-import { ArrowUp, MoveUpRight, CornerUpRight, X, Check } from 'lucide-react'
+import { useEffect, useMemo } from 'react'
+import { ArrowUp, X, Check } from 'lucide-react'
 import { Message, useChat } from 'ai/react'
 import { animateScroll } from 'react-scroll'
-import { Input } from '@/components/ui/input'
+import { useRouter } from 'next/navigation'
+import { ChatInput } from '@/components/ai/input'
 import { Button } from '@/components/ui/button'
 import { ChatMessage } from '@/components/ai/message'
-import { cn } from '@/lib/utils'
+import { cn, generateUniqueId } from '@/lib/utils'
+import { storeMessages } from '@/actions/storeMessages'
 
 interface Props {
+  id?: string
   history?: Message[]
+  className?: string
 }
 
 const scrollOptions = {
@@ -25,7 +29,7 @@ const features = [
   },
   {
     id: 1,
-    text: 'You want to explore challenging questions about non-trinitarian beliefs within Adventism in a kind, judgment-free environment, at your own pace.',
+    text: 'You want to explore challenging questions about non-trinitarian beliefs within Adventism in a kind, judgment-free environment, at your own pace, on-demand.',
   },
   {
     id: 2,
@@ -33,56 +37,43 @@ const features = [
   },
 ]
 
-const faqs = [
-  {
-    id: 0,
-    text: 'What is the Father and Son truth?',
-  },
-  {
-    id: 1,
-    text: 'Do you believe in the Holy Spirit?',
-  },
-  {
-    id: 3,
-    text: 'Do you believe that Jesus is God?',
-  },
-  {
-    id: 4,
-    text: 'Was Jesus was created?',
-  },
-]
+export function Chat({ id, history = [], className }: Props) {
+  const chatID = id || useMemo(() => generateUniqueId(), [])
+  const router = useRouter()
 
-export function Chat({ history = [] }: Props) {
   const { messages, input, handleInputChange, handleSubmit, isLoading, stop } =
     useChat({
+      id: chatID,
       initialMessages: history,
     })
+
+  const syncBeforeNavigateAsync = async () => {
+    if (!isLoading) {
+      if (messages.length === 2 && !id) {
+        await storeMessages(messages)
+        router.push(`/chat/${chatID}`)
+      }
+    }
+  }
 
   useEffect(() => {
     if (messages.length) {
       animateScroll.scrollToBottom(scrollOptions)
+      syncBeforeNavigateAsync()
     }
   }, [messages, isLoading])
 
   return (
-    <div className='isolate flex flex-col w-full md:max-w-lg py-12 mx-auto lg:dark:border-neutral-900 min-h-dvh'>
-      <div
-        aria-hidden='true'
-        className='fixed inset-x-0 -top-40 transform-gpu overflow-hidden blur-3xl sm:-top-80 -z-50'
-      >
-        <div
-          style={{
-            clipPath:
-              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-          }}
-          className='relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-blue-500 to-blue-800 opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]'
-        />
-      </div>
-
+    <div
+      className={cn(
+        'flex flex-col w-full md:max-w-lg py-12 mx-auto lg:dark:border-neutral-900 min-h-dvh',
+        className
+      )}
+    >
       {!messages.length && (
-        <div className='w-full antialiased'>
+        <>
           <section className='p-6 pb-8'>
-            <h1 className='mb-3 text-xl'>Hey there, welcome to ChatFAST!</h1>
+            <h1 className='mb-3 text-xl'>Welcome to ChatFAST!</h1>
 
             <p>
               This is a space for people to learn and chat about the Father and
@@ -100,46 +91,19 @@ export function Chat({ history = [] }: Props) {
             <h2 className=''>Who is this for?</h2>
 
             <ul className='mt-3'>
-              {features.map(({ id, text }) => {
-                return (
-                  <li className='flex items-start mt-4' key={id}>
-                    <Check className='mr-3 shrink-0 mt-1' size={18} />
+              {features.map(({ id, text }) => (
+                <li className='flex items-start mt-4' key={id}>
+                  <Check className='mr-3 shrink-0 mt-1' size={18} />
 
-                    <p className='text-neutral-600 dark:text-neutral-400'>
-                      {text}
-                    </p>
-                  </li>
-                )
-              })}
+                  <p className='text-neutral-600 dark:text-neutral-400'>
+                    {text}
+                  </p>
+                </li>
+              ))}
             </ul>
           </section>
-
-          <section className='bg-black/[3%] dark:bg-white/[3%] p-6 rounded-3xl mt-5 border'>
-            <h2 className=''>FAQs</h2>
-
-            <ul className='mt-3'>
-              {faqs.map(({ id, text }) => {
-                return (
-                  <li
-                    className='group flex items-center justify-between border-b p-2.5 last:border-none hover:rounded-lg hover:cursor-pointer border-dotted hover:bg-black/[3%] dark:hover:bg-white/[3%] transition-all'
-                    key={id}
-                  >
-                    <p className='text-neutral-600 dark:text-neutral-400 group-hover:text-blue-500 transition-colors'>
-                      {`${(id + 1).toString().padStart(1)}. ${text}`}
-                    </p>
-
-                    <MoveUpRight
-                      className='ml-3 shrink-0 mt-1 group-hover:text-blue-500 transition-colors'
-                      size={18}
-                    />
-                  </li>
-                )
-              })}
-            </ul>
-          </section>
-        </div>
+        </>
       )}
-
       <section className='p-6 pb-8'>
         {messages.map(({ id, role, content, ...props }) => (
           <ChatMessage
@@ -154,22 +118,13 @@ export function Chat({ history = [] }: Props) {
       </section>
 
       <form onSubmit={handleSubmit}>
-        <div className='fixed bottom-0 w-full md:max-w-lg flex items-center mb-5 -translate-x-1/2 left-1/2 px-4'>
-          <Input
-            className={cn(
-              'w-full h-12 px-6 border text-base bg-neutral-50/70 border-neutral-200 backdrop-blur-md rounded-xl placeholder:text-neutral-400 pr-14',
-              'dark:bg-white/[3%] dark:border-neutral-800 dark:text-neutral-300 dark:placeholder:text-neutral-500',
-              'hover:border-neutral-400'
-            )}
-            value={input}
-            placeholder='Ask me a question!'
-            onChange={handleInputChange}
-          />
+        <div className='fixed bottom-0 w-full md:max-w-xl flex items-center mb-5 -translate-x-1/2 left-1/2 md:px-8'>
+          <ChatInput input={input} handleInputChange={handleInputChange} />
 
           {isLoading ? (
             <Button
               type='button'
-              className='absolute right-8 rounded-full ml-2 w-7 h-7 p-0 shrink-0 bg-stone-700'
+              className='absolute md:right-14 right-5 rounded-full ml-2 w-7 h-7 p-0 shrink-0 bg-stone-700'
               onClick={stop}
             >
               <X size={15} />
@@ -178,7 +133,7 @@ export function Chat({ history = [] }: Props) {
             <Button
               type='submit'
               className={cn(
-                'absolute right-8 rounded-full ml-2 w-7 h-7 p-0 shrink-0 bg-stone-700 disabled:bg-stone-300',
+                'absolute md:right-14 right-5 rounded-full ml-2 w-7 h-7 p-0 shrink-0 bg-stone-700 disabled:bg-stone-300',
                 'dark:bg-neutral-50 dark:disabled:bg-neutral-600'
               )}
               disabled={!input || isLoading}
