3150a8d53141e295e237180d43c80c8f5ccdee76
joelriveradev <joelriveradev@gmail.com>
2024-09-19 11:35:13 -0500

    chore: logo style tweaks

--- src/components/app/logo.tsx
+++ src/components/app/logo.tsx
@@ -1,12 +1,21 @@
+import { cn } from '@/lib/utils'
 import { Zap } from 'lucide-react'
+
 import Link from 'next/link'
 
-export function Logo() {
+interface Props {
+  className?: string
+}
+export function Logo({ className }: Props) {
   return (
-    <Link href='/' className='flex items-center font-medium group' prefetch>
+    <Link
+      href='/'
+      className={cn('flex items-center font-medium group', className)}
+      prefetch
+    >
       ChatFAST{' '}
       <Zap
-        className='ml-0.5 text-blue-500 group-hover:text-white group-hover:rotate-180 transition-all'
+        className='ml-0.5 group-hover:rotate-180 transition-all group-hover:text-blue-500'
         size={18}
       />
     </Link>
